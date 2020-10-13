f6bca3cb93c352fa9ca214156eb28a46c99ce8a5
bararamadhan <bararamadhan@gmail.com>
2020-10-13 16:54:39 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ appName }}</h1>
+    <Game />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Game from "./components/Game.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Game,
+  },
+  data: function () {
+    return {
+      appName: "Speed Typing",
+    };
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Game.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="game">
+    <h2 v-bind:style="{ color: activeColor }">{{ word }}</h2>
+    <input v-model="gameinput" v-on:keyup.enter="entered()" placeholder="type here" />
+    <h3>{{ status }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Game",
+  props: {
+    appName: String,
+  },
+  data: function () {
+    return {
+      list: ['MAKAN','MINUM',''],
+      word: "MAKAN",
+      status: "",
+      gameinput: "",
+      activeColor: "black",
+    };
+  },
+  methods:{
+    entered(){
+      if(this.word.toUpperCase() == this.gameinput.toUpperCase()){
+        this.status = "CORRECT";
+        this.activeColor = "green";
+        this.gameinput= "";
+      } else{
+        this.status = "WRONG";
+        this.activeColor = "red";
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input{
+    width:100%;
+    background: transparent;
+    text-align: center;
+    font-size: 1.3em;
+    font-weight: bold;
+    outline: none;
+    border: none;
+    border-bottom: 1px solid black;
+    text-transform: uppercase;
+}
+</style>
